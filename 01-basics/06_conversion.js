7fe0e9943b48f2b5dfbd9715bae34b1fd8fccf05
Asghar Ali Sindhu <zainmathinter@gmail.com>
2023-12-25 23:42:09 +0500

    06_conversion is added

---
+++ 01-basics/06_conversion.js
@@ -0,0 +1,53 @@
+//  todo: output is a number
+// let score = "33"
+// let scoreInNumber = Number(score)
+// console.log(scoreInNumber);
+// console.log(typeof scoreInNumber);
+
+// ! scoreInNumber is NaN and however typeof scoreInNumber is number, which is wrong.
+// let score = "33abc"
+// let scoreInNumber = Number(score)
+//     console.log(scoreInNumber);
+// console.log(typeof scoreInNumber);
+
+// ? Which is again problematic, because it give us 0 which may not be the value of null
+// let score = null
+// let scoreInNumber = Number(score)
+//     console.log(scoreInNumber);
+// console.log(typeof scoreInNumber);
+
+// todo: Its output is NaN and typeof is again number
+// let score = undefined
+// let scoreInNumber = Number(score)
+//     console.log(scoreInNumber);
+// console.log(typeof scoreInNumber);
+
+//! In case of boolean value. Its output is 1 and typeof is number
+// let score = true
+// let scoreInNumber = Number(score)
+//     console.log(scoreInNumber);
+// console.log(typeof scoreInNumber);
+
+// ? Its output is NaN and typeof is a number
+//  let score = "Hitesh"
+// let scoreInNumber = Number(score)
+//     console.log(scoreInNumber);
+// console.log(typeof scoreInNumber);
+
+// ************String******************//
+// let score = 33
+// let scoreInString = String(score)
+// console.log(scoreInString);
+// console.log(typeof scoreInString);
+
+// ! boolean to String
+// let isLoggedIn = true
+// let isLoggedInString = String(isLoggedIn)
+// console.log(isLoggedInString);
+// console.log(typeof isLoggedInString);
+
+//todo: null to string
+// let score = null
+// let scoreInString = String(score)
+// console.log(scoreInString);
+// console.log(typeof scoreInString);
