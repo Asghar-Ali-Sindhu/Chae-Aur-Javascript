95a6e433707b3f3b5c97d059e0f19577e037efa8
Asghar Ali Sindhu <zainmathinter@gmail.com>
2023-12-25 22:34:49 +0500

    05-datatypes

---
+++ 01-basics/05_datatypes.js
@@ -0,0 +1,11 @@
+// todo: When we write "use strict" in the beginning, it means that all the code will be in newer version of javascript. However, these days it is not needed to write "use strict" and all the code is automatically written in newer version of JS
+// ! Data Types. Primitive: 
+/ Number. Its range is -(2 raise to the power 53 -1) to (2 raise to the power 53 -1) /
+/ BigInt /
+/ String - It is written between " " or ' '/
+/ Boolean - It has a value of true of false /
+/ Null - It is a standalone value i.e. empty value. When we check the typeof null, it is object. Some people says that it is the problem of language. /
+/ Undefined- typeof undefined is undefined /
+    / Symbol- It is used for uniqueness /
+// ! Data Types. Non - Primitive: 
+/ Object - /
